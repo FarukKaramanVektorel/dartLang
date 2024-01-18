f1b3f89d26c2cc36eaf4c16b353fe38d9be1b9b0
Faruk Karaman <101044778+Faruk-Karaman@users.noreply.github.com>
2024-01-19 00:48:07 +0300

    19-01-2024

---
+++ basic/hello_world.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  //print ile ekrana çıktı alınır
+  /*
+  çoklu yorum satırı
+  /** */ ile tutulur
+  */
+  print("Hello World!!!");
+}
---
+++ basic/variables.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  //Sayısal veri tipleri
+  int sayi = 45; // Tam sayı veri tipleri
+  print(sayi);
+  double ondalikliSayi = 45.125; //Ondalıklı sayı tipleri
+  print(ondalikliSayi);
+  num sayi1 = 45; // num veri tipinde hem ondalıklı hemde tam sayı tutulabilir.
+  print(sayi1);
+  num sayi2 = 45.785;
+  print(sayi2);
+
+  //Metin veri tipleri
+  String name = "Ahmet Keskin";
+  print(name);
+//mantıksal veri tipleri
+
+  bool durum = true;
+  print(durum);
+  //var ile değişken veri tipi belirtmeden atama yapılır ancak değişkene ilk atadığımız
+  //değere göre sonraki satırlarda diğer veri tipleri veremeyiiz.
+
+  var ad = "Ahmet Keser";
+  //ad=45; diyemeyiz
+  print(ad);
+
+  var sayi4 = 45;
+  // sayi4=45.56; hata verir
+  print(sayi4);
+}
